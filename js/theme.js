a2c29b21ea8450b4b7c44f7e913f2ce487e1fb49
AllirionX <44642633+AllirionX@users.noreply.github.com>
2018-12-24 16:55:37 +0100

    Small font-size fix + experience fix

--- js/theme.js
+++ js/theme.js
@@ -50,7 +50,7 @@ $(document).ready(function(){
 		setTimeout(function(){
 			$('body').removeClass('load');
 		}, 500);
-    }, 3800);
+    }, 2800);
 	
     //Init fullpage
 	$('#fullpage, #hub').fullpage({
